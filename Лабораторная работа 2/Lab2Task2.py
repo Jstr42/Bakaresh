eab814f16c2cf2369a9c1437c1419a0e1e60212b
Jstr42 <sergklimpz@gmail.com>
2024-10-27 21:40:14 +0300

    Add files via upload

---
+++ Лабораторная работа 2/Lab2Task2.py
@@ -0,0 +1,20 @@
+
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+
+
+required_capital = 0
+
+
+for month in range(months):
+    monthly_deficit = max(0, spend - salary)
+    required_capital += monthly_deficit
+    spend *= (1 + increase)
+
+
+required_capital = round(required_capital)
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", required_capital)
